10b33634a7418cd1fb7db39bd2b7dd1c9b0503cf
Santosh1978 <santosh33@gmail.com>
2018-05-18 21:53:48 +0530

    Completed code for chapter08

--- crimemap.py
+++ crimemap.py
@@ -4,6 +4,9 @@ from flask import render_template
 from flask import request
 import json
 import dbconfig
+import datetime
+import dateparser
+import string
 
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
@@ -13,13 +16,28 @@ else:
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['mugging','break-in']
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.ascii_letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
+
 
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home1.html", crimes=crimes)
+    return render_template("home1.html", crimes=crimes,categories=categories,error_message=error_message)
 # The below lines of code is commented from chapter 6 implementation
 # def home():
 #     try:
@@ -29,35 +47,57 @@ def home():
 #         data = None
 #     return render_template("home1.html", data=data)
 
-
-@app.route("/add", methods=["POST"])
-def add():
-    try:
-        data = request.form.get("userinput")
-        DB.add_input(data)
-    except Exception as e:
-        print(e)
-    return home()
-
-
-@app.route("/clear")
-def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print(e)
-    return home()
+# commented as below functions are not required at end of chapter 08
+# @app.route("/add", methods=["POST"])
+# def add():
+#     try:
+#         data = request.form.get("userinput")
+#         DB.add_input(data)
+#     except Exception as e:
+#         print(e)
+#     return home()
+#
+#
+# @app.route("/clear")
+# def clear():
+#     try:
+#         DB.clear_all()
+#     except Exception as e:
+#         print(e)
+#     return home()
 
 
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except:
+        error_message = "Latitude and Longitude have incorrect format"
+        return home(error_message)
+
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
+
+
+
+
+
+# def format_date(userdate):
+# date = dateparser.parse(userdate)
+# try:
+# return datetime.datetime.strftime(date, "%Y-%m-%d")
+# except TypeError:
+# return None
 
 
 if __name__ == '__main__':
--- dbhelper.py
+++ dbhelper.py
@@ -12,35 +12,36 @@ class DBHelper:
                                passwd=dbconfig.db_password,
                                db=database)
 
-    def get_all_inputs(self):
-        connection = self.connect()
-        try:
-            query = "SELECT description FROM crimes;"
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-            return cursor.fetchall()
-        finally:
-            connection.close()
+    # commented as below functions are not required at end of chapter 08
+    # def get_all_inputs(self):
+    #     connection = self.connect()
+    #     try:
+    #         query = "SELECT description FROM crimes;"
+    #         with connection.cursor() as cursor:
+    #             cursor.execute(query)
+    #         return cursor.fetchall()
+    #     finally:
+    #         connection.close()
 
-    def add_input(self, data):
-        connection = self.connect()
-        try:
-            query = "INSERT INTO crimes (description) VALUES (%s);"
-            with connection.cursor() as cursor:
-                cursor.execute(query, data)
-                connection.commit()
-        finally:
-            connection.close()
-
-    def clear_all(self):
-        connection = self.connect()
-        try:
-            query = "DELETE FROM crimes;"
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-                connection.close()
-        finally:
-            connection.close()
+    # def add_input(self, data):
+    #     connection = self.connect()
+    #     try:
+    #         query = "INSERT INTO crimes (description) VALUES (%s);"
+    #         with connection.cursor() as cursor:
+    #             cursor.execute(query, data)
+    #             connection.commit()
+    #     finally:
+    #         connection.close()
+    #
+    # def clear_all(self):
+    #     connection = self.connect()
+    #     try:
+    #         query = "DELETE FROM crimes;"
+    #         with connection.cursor() as cursor:
+    #             cursor.execute(query)
+    #             connection.close()
+    #     finally:
+    #         connection.close()
 
     def add_crime(self, category, date, latitude, longitude, desc):
         connection = self.connect()
